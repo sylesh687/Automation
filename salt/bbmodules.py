f3b2e5769955e3fc515f11371e565fcb44787d1b
Gurkhe Sylesh <timime687@gmail.com>
2017-10-12 15:19:15 +0530

    Added Cluster Code

--- salt/bbmodules.py
+++ salt/bbmodules.py
@@ -5,17 +5,160 @@ import logging
 from subprocess import Popen, PIPE
 import shlex
 
+from pylxd import Client
+import re
+
+
 '''
 	Runs a linux command and returns file descriptors
 '''
+
+
 def runCmd(cmd):
 	
 
 	cmdargs=shlex.split(cmd)
-	Execute_Cmd=Popen(cmdargs, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+	Execute_Cmd=Popen(cmdargs, stdin=PIPE, stdout=PIPE, stderr=PIPE,shell=True)
 	output, err = Execute_Cmd.communicate()
 	rc=Execute_Cmd.returncode
 
 
 	return output,err,rc
+'''
+	Creates n numbers of Containers provided with Prefix 
+
+'''
+
+def createContainer(number,containerPrefix="BB"):
+	
+	conlist=[]
+
+	for con in range(int(number)):
+		
+		conname="%s%s" % (containerPrefix,con)
+		command="lxc launch ubuntu: %s "% (conname)
+		
+		output,err,rc=runCmd(command)
+
+		if rc==0:
+			conlist.append(conname)
+			print output
+		else: 
+			print err
+	
+	return conlist
+
+
+'''
+  	This Function gets the Number of running Containers on your system
+
+'''
+def getContainers(con):
+	
+	conlist=[]
+	
+	for container in (con.containers.all()):
+		if (((container.status).upper()) == "RUNNING"):
+			
+			conlist.append(container.name)
+	
+	return conlist
+
+
+
+'''
+	This function is used to Delete the Container list with Some prefix
+
+'''
+
+def delContainers(prefex,conn):
+	
+	exp=re.compile("%s.*"%prefex)
+	stopcommand="lxc stop %s "
+	
+	runningContainers=getContainers(conn)
+
+	targetContainer=filter(exp.match,runningContainers)
+
+	if len(targetContainer) == 0:
+
+		print "We Dont Have Any Containers with supplied Prefex %s  , Please Try With Something Else" %(prefex)
+
+	else:
+		for tc in targetContainer:
+			try:
+				stopcommand="lxc stop %s " % (tc)
+				print "Stopping %s, By Executing %s" %(tc,stopcommand)
+				output,err,rc=runCmd(stopcommand)
+
+				if rc==0:
+					print "SuccessFully Stopped %s " %(tc)
+
+					print output
+				else:
+					print err
+
+				delcommand="lxc delete %s " % (tc)
+
+				print "Deleting %s, By Executing %s" %(tc,delcommand)
+
+				output , err , rc= runCmd(delcommand)
+
+				if rc==0 :
+					print "SuccessFully Deleted %s " %(tc)
+					print output 
+				else:
+					print err
+
+			
+			except Exception as e:
+				print e
+
+
+
+'''
+	This function runs linux commannd on n running containers
+
+'''
+
+def executeCmd(containerList,command):
+
+
+	flag=0
+	msg=''
+	
+	if len(containerList)==0:
+		
+		flag=1
+		msg= "Please input atleast single list of container"
+
+
+	else:
+
+		for container in containerList:
+
+
+			runCommand="lxc exec %s -- %s" %(container,command)
+			print "Running %s on %s "% (runCommand,container)
+
+			try:
+
+				output,err,rc=runCmd(runCommand)
+
+				if rc==0:
+					print output
+					print "Successfully executed %s  on %s " %(runCommand,container)
+
+				else: 
+					print err
+
+			except Exception as e:
+				print e
+		
+		flag=0
+		msg="Successfully run %s in %s" %(command,containerList)
+
+	return flag, msg
+
+
 
---
+++ salt/cluster.py
@@ -0,0 +1,137 @@
+
+from bbmodules import createContainer as cc
+from bbmodules import executeCmd as ec
+import re
+import sys
+
+
+def gatherInput():
+	clustnum=1
+
+	try:
+		
+		clustnum=input("Enter the Number Of Nodes In Salt-Cluster : -- ")
+		clustnum=int(clustnum)
+
+		rc,msg=Validation(clustnum)
+
+		if rc==0:
+			print msg
+		else:
+			print msg
+			sys.exit()
+		
+	except NameError as e:
+		print "Please Enter a valid interger as %s " %(e)
+
+	except ValueError as e:
+		print e
+
+	clustname=raw_input("Enter the ClusterName: -- ")
+
+	if not re.match("^[a-z]*$",clustname):
+		 print "Please Enter  a Valid ClusterName [ Only letters a-z allowed! ]"
+		 sys.exit()
+	
+	elif len(clustname)>10:
+		print "ClusterName should not be bigger than 8 chars"
+		sys.exit()
+
+	
+	return clustnum , clustname
+
+
+
+def Validation(clustnum):
+
+	'''
+
+		flag=0 means true and 1 means false
+
+	'''
+	flag=0
+	flagmsg=''
+	
+	if clustnum<=0 or clustnum > 15 :
+		flag=1
+		flagmsg="Input Should Be Other than 0 Nonegative numbers lesser than 15"
+	else:
+		flag=0
+		flagmsg="Successfully Validated"
+
+	return flag , flagmsg		
+
+
+
+def getMasterMinion():
+	
+	minion=[]
+	num,name=gatherInput()
+	
+	'''
+		 creating containers
+
+	'''
+	conlist=cc(num,name)
+	master=''
+	minion=[]
+	if len(conlist) == 1:
+		print "Single Node Cluster"
+		master=conlist[0]
+		minion=master
+	
+	else:
+		master=conlist[0]
+		minion=conlist[1:]
+
+	
+	return master,minion
+
+
+
+
+
+def settingupminion(minionlist):
+	'''
+		Clone the Git Repository
+
+	'''
+	update="apt-get update"
+	install_python="apt-get install python-minimal -y "
+	clone="git clone https://github.com/sylesh687/Automation.git"
+	install_master="python  Automation/salt/salt.py 16 master"
+
+	rc,msg=ec(minionlist,update)
+	rc,msg=ec(minionlist,install_python)
+	
+	if rc==0:
+		print msg
+	else: 
+		print msg
+
+	rc,msg=ec(minionlist,clone)
+	
+	if rc==0:
+		print msg
+	else: 
+		print msg
+
+
+	rc,msg=ec(minionlist,install_master)
+	
+	if rc==0:
+		print msg
+	else: 
+		print msg
+
+
+
+def main():
+	master,minion= getMasterMinion()
+	print master
+	print (settingupminion(minion))
+
+
+
+if __name__=="__main__":
+	main()
